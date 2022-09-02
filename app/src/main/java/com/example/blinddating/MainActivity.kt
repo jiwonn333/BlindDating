9e0076bce7b84cfbcde0671f6fce710ba0c70546
jiwonn333 <84057628+jiwonn333@users.noreply.github.com>
2022-09-02 17:28:22 +0900

    메인 슬라이딩 화면 구현 commit

---
+++ app/src/main/java/com/example/blinddating/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.blinddating
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.blinddating.slider.CardStackAdapter
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.CardStackView
+import com.yuyakaido.android.cardstackview.Direction
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var cardStackAdapter: CardStackAdapter
+    // RecycelrView 만들 때 LayoutManager(LinearLayout, GridLayout)과 같은
+    lateinit var manager : CardStackLayoutManager
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cardStackView = findViewById<CardStackView>(R.id.cardStackView)
+
+        manager = CardStackLayoutManager(baseContext, object : CardStackListener{
+            override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+            }
+
+            override fun onCardSwiped(direction: Direction?) {
+
+            }
+
+            override fun onCardRewound() {
+
+            }
+
+            override fun onCardCanceled() {
+
+            }
+
+            override fun onCardAppeared(view: View?, position: Int) {
+
+            }
+
+            override fun onCardDisappeared(view: View?, position: Int) {
+
+            }
+
+        })
+
+        // 문자열 임시 생성 (items 임시 생성)
+        val testList = mutableListOf<String>()
+        testList.add("a")
+        testList.add("b")
+        testList.add("c")
+
+        cardStackAdapter = CardStackAdapter(baseContext, testList)
+        cardStackView.layoutManager = manager
+        cardStackView.adapter = cardStackAdapter
+    }
+}
