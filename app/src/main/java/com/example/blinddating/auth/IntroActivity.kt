3c5488026d8018099ba33fde2294cc649cf19c11
myWon <sjwon113@naver.com>
2022-09-07 16:51:03 +0900

    회원가입) Join(회원가입) 화면 꾸미기

--- app/src/main/java/com/example/blinddating/auth/IntroActivity.kt
+++ app/src/main/java/com/example/blinddating/auth/IntroActivity.kt
@@ -1,12 +1,24 @@
 package com.example.blinddating.auth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.example.blinddating.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        // 1. gradle에  id 'kotlin-android-extensions' 추가하면
+        // btn_join.setOnClickListener() 로 바로 사용 가능
+
+        // val btnJoin : Button = findViewById(R.id.btn_join)
+        val btnJoin = findViewById<Button>(R.id.btn_join)
+        btnJoin.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/blinddating/auth/JoinActivity.kt
@@ -0,0 +1,16 @@
+package com.example.blinddating.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.blinddating.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+
+
+    }
+}
